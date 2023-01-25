7afa856f6f16d9cfac6113ff8943a39c3f559f4f
nahidul77 <nahidul.mee@gmail.com>
2023-01-25 23:31:24 +0600

    random hex color generate

--- 01_Counter_app/script.js
+++ 01_Counter_app/script.js
@@ -22,27 +22,27 @@
 const count = document.querySelector(".count");
 const buttons = document.querySelector(".buttons");
 
-buttons.addEventListener('click', (e)=>{
-    if(e.target.classList.contains('add')){
+buttons.addEventListener('click', (e) => {
+    if (e.target.classList.contains('add')) {
         count.innerHTML++;
         setColor();
     }
-    if(e.target.classList.contains('reset')){
+    if (e.target.classList.contains('reset')) {
         count.innerHTML = 0;
         setColor();
     }
-    if(e.target.classList.contains('subtract')){
+    if (e.target.classList.contains('subtract')) {
         count.innerHTML--;
         setColor();
     }
 });
 
-function setColor() { 
-    if(count.innerHTML > 0){
+function setColor() {
+    if (count.innerHTML > 0) {
         count.style.color = "yellow";
-    }else if(count.innerHTML < 0){
+    } else if (count.innerHTML < 0) {
         count.style.color = "orangered";
-    }else{
+    } else {
         count.style.color = "#fff";
     }
- }
+}
